518df330397f61349dbb6aac151b8ec2a74c8199
Evgheni Alexeev <evgheni.alexeev@petrofac.com>
2024-12-04 18:50:04 +0200

    fixed middleware

--- src/WorkerNode/Function1.cs
+++ src/WorkerNode/Function1.cs
@@ -1,10 +1,12 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Security.Claims;
 
 namespace WorkerNode
@@ -15,29 +17,38 @@ namespace WorkerNode
 
         public Function1(ILogger<Function1> logger)
         {
-            _logger = logger;
+            //_logger = logger;
         }
 
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req, FunctionContext executionContext)
+        [Function(nameof(Function1))]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req, FunctionContext executionContext)
         {
+            var _logger = executionContext.GetLogger<Function1>();
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var authHeader = req.Headers["Authorization"].FirstOrDefault();
-            if (authHeader != null && authHeader.StartsWith("Bearer "))
-            {
-                var token = authHeader.Substring("Bearer ".Length).Trim();
+            //var authHeader = req.Headers["Authorization"].FirstOrDefault();
+            //if (authHeader != null && authHeader.StartsWith("Bearer "))
+            //{
+            //    var token = authHeader.Substring("Bearer ".Length).Trim();
 
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var jwtToken = tokenHandler.ReadJwtToken(token);
+            //    var tokenHandler = new JwtSecurityTokenHandler();
+            //    var jwtToken = tokenHandler.ReadJwtToken(token);
 
-                var claims = jwtToken.Claims.Select(c => new { c.Type, c.Value }).ToList();
-                // Log or use the claims as needed
-                _logger.LogInformation("User claims: {Claims}", claims);
-            }
+            //    var claims = jwtToken.Claims.Select(c => new { c.Type, c.Value }).ToList();
+            //    // Log or use the claims as needed
+            //    _logger.LogInformation("User claims: {Claims}", claims);
+            //}
 
 
-            return new OkObjectResult("Welcome to Azure Functions!");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            // Set a context item the middleware can retrieve
+            executionContext.Items.Add("functionitem", "Hello from function!");
+
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Welcome to .NET 6!!");
+
+            return response;
         }
     }
 }
--- src/WorkerNode/Program.cs
+++ src/WorkerNode/Program.cs
@@ -5,17 +5,16 @@ using Microsoft.Extensions.Hosting;
 using WorkerNode.Authentication;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
     .ConfigureFunctionsWorkerDefaults(worker =>
     {
         worker.UseMiddleware<JwtValidationMiddleware>();
     })
+    .ConfigureServices(services =>
+    {
+        //services.AddApplicationInsightsTelemetryWorkerService();
+        //services.ConfigureFunctionsApplicationInsights();
+    })
+    //.ConfigureFunctionsWebApplication()
     .Build();
 
-host.Run();
+await host.RunAsync();
